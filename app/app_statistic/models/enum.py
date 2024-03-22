6229efab75734651161a8ac4cecaa79c3cafc313
MaratGumerov <gumerovm74@gmail.com>
2024-03-22 19:51:41 +0300

    m statistic add model

---
+++ app/app_statistic/models/enum.py
@@ -0,0 +1,14 @@
+import enum
+
+
+class DepartmentsEnum(enum.Enum):
+    """
+    Enum значений департаментов.
+    """
+
+    MARKETING = "marketing"
+    SALES = "sales"
+    INFORMATIONAL = "informational"
+    SURNEY = "survey"
+    COMMERTIONAL_SURVEY = "commercial_survey"
+    SERVICE = "service"
--- app/app_statistic/models/models.py
+++ app/app_statistic/models/models.py
@@ -1,7 +1,20 @@
 from app.app_statistic.models.mixin import TimestampMixin
 from app.app_statistic.models.base import ABCBase
+from sqlalchemy.orm import Mapped, mapped_column
+from app.app_statistic.models.enum import DepartmentsEnum
+
 
 class Statistic(ABCBase, TimestampMixin):
     """
-    Модель таблицы базы данных идей.
+    Модель таблицы базы данных Статистики.
     """
+
+    __tablename__ = 'statistic'
+
+    department: Mapped[DepartmentsEnum] = mapped_column(default=DepartmentsEnum.MARKETING)
+    mass: Mapped[int]
+    personal: Mapped[int]
+    informational: Mapped[int]
+    survey: Mapped[int]
+    commercial_survey: Mapped[int]
+    service: Mapped[int]
